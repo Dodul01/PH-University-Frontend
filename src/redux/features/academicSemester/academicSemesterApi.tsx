162e405541cd2f175e165f256a3ce120b8fee7f7
Dodul01 <mhdodul299@gmail.com>
2025-01-17 20:56:40 +0600

    Module 29 completed

--- src/redux/features/academicSemester/academicSemesterApi.tsx
+++ src/redux/features/academicSemester/academicSemesterApi.tsx
@@ -1,14 +1,42 @@
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { TQueryParam, TResponseRedux } from "../../../types/global";
 import { baseApi } from "../../api/baseApi";
 
 const academicSemesterApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllSemesters: builder.query({
-      query: () => ({
-        url: "/academic-semesters",
-        method: "GET",
+      query: (args) => {
+        console.log(args);
+        const params = new URLSearchParams();
+
+        if (args) {
+          args.forEach((item: TQueryParam) => {
+            params.append(item.name, item.value as string);
+          });
+        }
+
+        return {
+          url: "/academic-semesters",
+          method: "GET",
+          params: params,
+        };
+      },
+      transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+        return {
+          data: response.data,
+          meta: response.meta,
+        };
+      },
+    }),
+    addAcademicSemester: builder.mutation({
+      query: (data) => ({
+        url: "/academic-semesters/create-academic-semester",
+        method: "POST",
+        body: data,
       }),
     }),
   }),
 });
 
-export const { useGetAllSemestersQuery } = academicSemesterApi;
+export const { useGetAllSemestersQuery, useAddAcademicSemesterMutation } =
+  academicSemesterApi;
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,4 +1,9 @@
+import AcademicDepartment from "../pages/AdminPages/AcademicManagement/AcademicDepartment";
+import AcademicFaculty from "../pages/AdminPages/AcademicManagement/AcademicFaculty";
 import AcademicSemester from "../pages/AdminPages/AcademicManagement/AcademicSemester";
+import CreateAcademicDepartment from "../pages/AdminPages/AcademicManagement/CreateAcademicDepartment";
+import CreateAcademicFaculty from "../pages/AdminPages/AcademicManagement/CreateAcademicFaculty";
+import CreateAcademicSemester from "../pages/AdminPages/AcademicManagement/CreateAcademicSemester";
 import AdminDashboard from "../pages/AdminPages/AdminDashboard/AdminDashboard";
 import CreateAdmin from "../pages/AdminPages/CreateAdmin/CreateAdmin";
 import CreateFaculty from "../pages/AdminPages/CreateFaculty/CreateFaculty";
@@ -14,9 +19,34 @@ export const adminPaths = [
     name: "Academic Management",
     children: [
       {
+        name: "Create A. Semester",
+        path: "create-academic-semester",
+        element: <CreateAcademicSemester />,
+      },
+      {
         name: "Academic Semester",
         path: "academic-semester",
         element: <AcademicSemester />,
+      },
+      {
+        name: "Create A. Faculty",
+        path: "create-academic-faculty",
+        element: <CreateAcademicFaculty />,
+      },
+      {
+        name: "Academic Faculty",
+        path: "academic-faculty",
+        element: <AcademicFaculty />,
+      },
+      {
+        name: "Create A. Department",
+        path: "create-academic-department",
+        element: <CreateAcademicDepartment />,
+      },
+      {
+        name: "Academic Department",
+        path: "academic-department",
+        element: <AcademicDepartment />,
       },
     ],
   },
