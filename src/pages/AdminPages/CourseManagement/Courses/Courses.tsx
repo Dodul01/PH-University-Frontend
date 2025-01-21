2b2c742aee7ce95a6da30153371376db7f1fc017
Dodul01 <mhdodul299@gmail.com>
2025-01-21 14:03:55 +0600

    create course and show course on table added

--- src/pages/AdminPages/CourseManagement/Courses/Courses.tsx
+++ src/pages/AdminPages/CourseManagement/Courses/Courses.tsx
@@ -1,7 +1,100 @@
-const Courses = () => {
-  return (
-    <div>Courses</div>
-  )
-}
+import { Button, Modal, Table } from 'antd';
+import { useState } from 'react';
+import { useAddFacultiesMutation, useGetAllCoursesQuery } from '../../../../redux/features/courseManagement/courseManagement.api';
+import { useGetAllFacultiesQuery } from '../../../../redux/features/userManagement/userManagement.api';
+import PHForm from '../../../../components/form/PHForm';
+import PHSelect from '../../../../components/form/PHSelect';
 
-export default Courses
+const Courses = () => {
+  // const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+  const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    code: `${prefix}${code}`,
+  }));
+
+  const columns = [
+    {
+      title: 'Title',
+      key: 'title',
+      dataIndex: 'title',
+    },
+    {
+      title: 'Code',
+      key: 'code',
+      dataIndex: 'code',
+    },
+    {
+      title: 'Action',
+      key: 'x',
+      render: (item) => {
+        return <AddFacultyModal facultyInfo={item} />;
+      },
+    },
+  ];
+
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+    />
+  );
+};
+
+const AddFacultyModal = ({ facultyInfo }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: facultiesData } = useGetAllFacultiesQuery(undefined);
+  const [addFaculties] = useAddFacultiesMutation();
+
+  const facultiesOption = facultiesData?.data?.map((item) => ({
+    value: item._id,
+    label: item.fullName,
+  }));
+
+  const handleSubmit = (data) => {
+    const facultyData = {
+      courseId: facultyInfo.key,
+      data,
+    };
+
+    console.log(facultyData);
+
+    addFaculties(facultyData);
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm onSubmit={handleSubmit}>
+          <PHSelect
+            mode="multiple"
+            options={facultiesOption}
+            name="faculties"
+            label="Faculty"
+          />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
+  );
+};
+
+export default Courses;
--- src/pages/AdminPages/CourseManagement/CreateCourse/CreateCourse.tsx
+++ src/pages/AdminPages/CourseManagement/CreateCourse/CreateCourse.tsx
@@ -1,5 +1,73 @@
+import { FieldValues, SubmitHandler } from 'react-hook-form';
+import { Button, Col, Flex } from 'antd';
+import { toast } from 'sonner';
+import { useAddCourseMutation, useGetAllCoursesQuery } from '../../../../redux/features/courseManagement/courseManagement.api';
+import { TResponse } from '../../../../types/global';
+import PHForm from '../../../../components/form/PHForm';
+import PHInput from '../../../../components/form/PHInput';
+import PHSelect from '../../../../components/form/PHSelect';
+
+
 const CreateCourse = () => {
-  return <div>CreateCourse</div>;
+  const [createCourse] = useAddCourseMutation();
+  const { data: courses } = useGetAllCoursesQuery(undefined);
+
+  const preRequisiteCoursesOptions = courses?.data?.map((item) => ({
+    value: item._id,
+    label: item.title,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading('Creating...');
+    console.log(data);
+    
+    const courseData = {
+      ...data,
+      code: Number(data.code),
+      credits: Number(data.credits),
+      isDeleted: false,
+      preRequisiteCourses: data.preRequisiteCourses
+        ? data.preRequisiteCourses?.map((item) => ({
+            course: item,
+            isDeleted: false,
+          }))
+        : [],
+    };
+
+    console.log(courseData);
+
+    try {
+      const res = (await createCourse(courseData)) as TResponse<any>;
+      console.log(res);
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success('Semester created', { id: toastId });
+      }
+    } catch (err) {
+      toast.error('Something went wrong', { id: toastId });
+    }
+  };
+
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="title" label="Title" />
+          <PHInput type="text" name="prefix" label="Prefix" />
+          <PHInput type="text" name="code" label="Code" />
+          <PHInput type="text" name="credits" label="Credits" />
+          <PHSelect
+            mode="multiple"
+            options={preRequisiteCoursesOptions}
+            name="preRequisiteCourses"
+            label="preRequisiteCourses"
+          />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
 };
 
 export default CreateCourse;
